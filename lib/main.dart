16acf761f953e8004c32751f32782299ce798087
Danielkvist <danielkvist@protonmail.com>
2020-05-12 10:22:00 +0200

    add notes about functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
+// Dart functions
+//  -> void getMilk() { /*functionality*/ };
+// 
+// We can specify an "input" (argument):
+//  -> void getMilk(int n) { print('number: $n'); }
+//  -> getMilk(2);
+// You have to specify the data type of the argument and the name.
+// We can name our parameters:
+//  -> void greet({ String greeting, String personToGreet }) {...}
+//  -> greet(greeting: 'How are u', personToGreet: 'Jackie');
+
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
