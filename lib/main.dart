f0d931b083a83bd251f3212a2ce41fdfd8e25c92
Danielkvist <danielkvist@protonmail.com>
2020-05-12 10:06:09 +0200

    add rest of notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
-// What are Flutter & Dart packages?
-//
-// Flutter packages are Open Source libraries of code
-// that other people had created that you can use
-// in your project.
-// To add the depedency to your project you can
-//    * simply add the name (english_words).
-//    * add it with the version number (english_words: ^3.1.5).
-
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                // This is all we need to play local audio
-                final player = AudioCache();
-
-                // Audioplayers assumes that your files
-                // are in assets/
-                player.play('note2.wav');
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
